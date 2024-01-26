5129ce5f6e541fd781d842eb63a716f33aba8db0
abcdaramgi <wwa1102@naver.com>
2024-01-27 00:09:42 +0900

    [feat] - 구글 map 작동 mapview

--- src/screens/Map/index.tsx
+++ src/screens/Map/index.tsx
@@ -1,12 +1,14 @@
 import React, {FC} from 'react';
 import {View, StyleSheet} from 'react-native';
 import MapView from 'react-native-maps';
+import {PROVIDER_GOOGLE} from 'react-native-maps';
 
 const MapScreen: FC = () => {
   return (
     <View style={styles.viewContainer}>
       <MapView
-        style={styles.viewContainer}
+        style={styles.mapContainer}
+        provider={PROVIDER_GOOGLE}
         initialRegion={{
           latitude: 37.78825,
           longitude: -122.4324,
@@ -21,6 +23,11 @@ const MapScreen: FC = () => {
 const styles = StyleSheet.create({
   viewContainer: {
     flex: 1,
+  },
+  mapContainer: {
+    flex: 1,
+    width: '100%',
+    height: '100%',
   },
 });
 
